a407f013c7eb2ec036e734d6331523ca21a2d8f3
Eric Ene <123761606+eric-ene@users.noreply.github.com>
2024-11-20 12:01:16 -0500

    start adding listening

--- src/data/packet.rs
+++ src/data/packet.rs
@@ -1,29 +1,5 @@
-// header data:
-// byte 0: type of message
-//  0xF0 -> name assignment
-//  0xF1 -> message send
-// bytes 1-7 (inclusive): reserved for future use
-// everything else: message data
-// PACKET MUST END WITH EOF (0x04)
+use std::net::TcpStream;
 
-#[repr(u8)]
-pub enum PacketType {
-  NameAssign = 0xF0,
-}
-
-#[repr(u8)]
-pub enum PacketSymbols {
-  Eof = 0x04,
-}
-
-pub fn create_id_packet(id: String) -> Vec<u8> {
-  let mut packet: Vec<u8> = vec![0u8; 8]; // header
-  packet[0] = PacketType::NameAssign as u8;
-  
-  for byte in id.as_bytes() {
-    packet.push(*byte);
-  }
-  
-  packet.push(PacketSymbols::Eof as u8);
-  return packet;
+pub trait ServerPackets {
+  fn forward(stream: &mut TcpStream) -> std::io::Result<()>;
 }
--- src/threading.rs
+++ src/threading.rs
@@ -1,14 +1,15 @@
 use std::collections::HashMap;
-use std::io::Write;
-use std::net::{SocketAddr, TcpStream};
+use std::io::{Read, Write};
+use std::net::{Shutdown, SocketAddr, TcpStream};
 use std::sync::{Arc, LockResult, Mutex};
 use std::thread;
 use std::thread::JoinHandle;
+use chat_shared::packet::assign::{AssignPacket};
+use chat_shared::packet::{Packet, ProcessedPacket};
 use crate::data::generate_unique_id;
-use crate::data::packet::create_id_packet;
 
 pub fn thread_stream(mut stream: TcpStream, taken: Arc<Mutex<HashMap<String, SocketAddr>>>) -> JoinHandle<()> {
-  thread::spawn(move || {
+  return thread::spawn(move || {
     let peer_addr = stream.peer_addr().unwrap();
     println!("Connection from {}", peer_addr);
     
@@ -20,15 +21,65 @@ pub fn thread_stream(mut stream: TcpStream, taken: Arc<Mutex<HashMap<String, Soc
       }
     };
     
-    let packet = create_id_packet(id);
+    let assign = AssignPacket {
+      content: id,
+    };
+    
+    let packet = ProcessedPacket::new_raw(ProcessedPacket::Assign(assign));
     match stream.write_all(packet.as_slice()) {
       Ok(_) => {
         println!("Successfully sent packet to {}", peer_addr);
       }
       Err(e) => {
-        println!("Error writing to stream: {}", e);
+        println!("Error writing to stream (connection closed): {}", e);
         return;
       }
     }
-  })
+    
+    // main event loop
+    // loop {
+    //   let mut buf = Vec::new();
+    //   let n_read = match stream.read_to_end(&mut buf) {
+    //     Ok(n) => n,
+    //     Err(e) => {
+    //       println!("error reading from stream: {}", e);
+    //       continue;
+    //     }
+    //   };
+    //   
+    //   if n_read == 0 {
+    //     println!("connection closed.");
+    //     return;
+    //   }
+    // 
+    //   if n_read < 8 {
+    //     println!("mangled packet! size {} is too small!", n_read);
+    //     continue;
+    //   }
+    //   
+    //   let packet = Packet::from_bytes(buf);
+    //   
+    //   // deserialize packet
+    //   let processed = match packet.process() {
+    //     Ok(processed) => processed,
+    //     Err(e) => {
+    //       println!("couldn't process packet: {}", e);
+    //       continue;
+    //     }
+    //   };
+    //   
+    //   // do stuff with the processed packet
+    //   match processed {
+    //     ProcessedPacket::Message(packet) => {
+    //       
+    //     }
+    //     ProcessedPacket::Handshake(packet) => {
+    //       
+    //     }
+    //     packet => {
+    //       println!("ignoring packet: {:?}", packet);
+    //     }
+    //   }
+    // }
+  });
 }
